7454df446c1eed95abc24f9fba81df2e0c887a69
Mac <mac@MacBook-Pro-Mac.local>
2020-08-03 19:39:10 +0300

    hw

---
+++ goit-react-hw-01-components/src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import tableStyle from "./transactionHistory.module.css";
+import PropTypes from "prop-types";
+
+const TransactionHistory = ({ items }) => (
+  <table className={tableStyle.transactionHistory}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {items.map((item) => (
+        <tr key={item.id}>
+          <td>{item.type}</td>
+          <td>{item.amount}</td>
+          <td>{item.currency}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      type: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TransactionHistory;
